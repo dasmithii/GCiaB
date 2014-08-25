70d6cc4fcee82f24d167177204b089d51196a9c6
Adam Smith <iihtimsad@gmail.com>
2014-08-25 00:14:38 -0400

    Prepared for open sourcing.

--- sources/GCiaB.h
+++ sources/GCiaB.h
@@ -1,28 +1,68 @@
-#ifndef GCIAB_API
-#define GCIAB_API
+#ifndef GC_MARK_AND_SWEEP
+#define GC_MARK_AND_SWEEP
+#include <stddef.h>
+#include <stdint.h>
+#include <limits.h>
+// This file implements mark and sweep garbage collection on top of
+// malloc and free, such that all standard libc allocation functions
+// may be used alongside ms_allocation functions without interference.
+//
+// Note: at this point, functions provided here are neither reentrant 
+// nor thread safe. Do not expect otherwise.
 
 
-// GC algorithms.
-#define MARK_AND_SWEEP 0
-#define REFERENCE_COUNTING 1
-#define THREE_COLOR 2
+// data prepended before every allocation
+typedef struct MSHeader {
+	struct MSHeader *next;
+	void (*foreach)(void*, void(*)(void*));
+	unsigned int marked : 1;
+	unsigned int rooted : 1;
+	unsigned int padding : CHAR_BIT - 3;
+	unsigned int barrier : 1;
+} MSHeader;
 
 
-// Change this line for a different default collector. Note
-// that each alorithm is available regardless, but X GC type
-// is more conveniently usable if specified here.
-#define DEFAULT_COLLECTOR MARK_AND_SWEEP
+// represents a full garbage collect
+typedef struct {
+	MSHeader *firstHeader;
+	MSHeader *lastHeader;
+	size_t unfreedAllocations;
+} MSCollector;
 
 
-// Arrange some convenience functions for the default collector.
-#if DEFAULT_COLLECTOR == MARK_AND_SWEEP
-#include "mark-and-sweep.h"
-#elif DEFAULT_COLLECTOR == REFERENCE_COUNTING
-#include "reference-counting.h"
-#elif DEFAULT_COLLECTOR == THREE_COLOR
-#include "three-color.h"
-#endif
+// not sure if portable
+#define MS_ALIGNMENT_OF(type)  \
+	offsetof( struct {         \
+			  	char x;        \
+			  	type member;   \
+		      }, member)
 
 
-// End include gaurd.
+// main interface
+#define ms_allocation(type, foreach)  ms_allocate(sizeof(type), MS_ALIGNMENT_OF(type), foreach)
+#define ms_array(type, size, foreach) ms_allocate(size * sizeof(type), MS_ALIGNMENT_OF(type), foreach)
+void ms_sweep();  
+void ms_clear();  
+void ms_debug();
+size_t ms_unfreed();
+void ms_root(void*);
+void ms_unroot(void*);
+
+
+// interface for non-global allocators
+#define ms_allocation_(gc, type, foreach)  ms_allocate_(gc, sizeof(type), MS_ALIGNMENT_OF(type), foreach)
+#define ms_array_(gc, type, size, foreach) ms_allocate_(gc, size * sizeof(type), MS_ALIGNMENT_OF(type), foreach)
+void ms_sweep_(MSCollector*);  
+void ms_clear_(MSCollector*);  
+void ms_debug_(MSCollector*);
+void ms_init_(MSCollector*);
+
+
+
+// allocate with given size and alignment
+void *ms_allocate(size_t, size_t, void (*)(void*, void(*)(void*)));
+void *ms_allocate_(MSCollector*, size_t, size_t, void (*)(void*, void(*)(void*)));
+
+
+
 #endif
